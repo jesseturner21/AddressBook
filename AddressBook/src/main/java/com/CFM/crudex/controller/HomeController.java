01296c3f048e879ebbe21122e69879e0520b75c4
jesseturner21 <jesseturner2021@gmail.com>
2023-10-04 12:52:51 -0700

    Bootstrap - Address book display

--- AddressBook/src/main/java/com/CFM/crudex/controller/HomeController.java
+++ AddressBook/src/main/java/com/CFM/crudex/controller/HomeController.java
@@ -1,11 +1,21 @@
 package com.CFM.crudex.controller;
 
+
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.CFM.crudex.entity.Person;
+import com.CFM.crudex.service.PersonServiceImpl;
+
 @Controller
 public class HomeController {
+	
+	@Autowired
+	PersonServiceImpl service;
     @GetMapping("/")
     public String displayHome(Model model) {
     	
@@ -16,7 +26,12 @@ public class HomeController {
     @GetMapping("/book")
     public String displayBook(Model model) {
     	
-    		model.addAttribute("message","Address Book");
+    		//get all people from address book 
+    	List<Person> persons = service.getAllPersons();
+    	System.out.print(persons);
+    	model.addAttribute("persons",persons);
+    	model.addAttribute("message","Address Book");
+    		
     	
         return "book";
     }
