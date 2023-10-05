b0283d7be8ccb6165526622a818507e1399a38b8
jesseturner21 <jesseturner2021@gmail.com>
2023-10-04 18:36:49 -0700

    delete functionality

--- AddressBook/src/main/java/com/CFM/crudex/controller/HomeController.java
+++ AddressBook/src/main/java/com/CFM/crudex/controller/HomeController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -65,6 +66,18 @@ public class HomeController {
     	else {
     		service.updatePerson(person, person.getId());
     	}
+    	
+    	List<Person> persons = service.getAllPersons();
+    	model.addAttribute("persons",persons);
+    	model.addAttribute("message","Address Book");
+    	
+    	return "book";
+    }
+    
+    @GetMapping("/delete/{id}")
+    public String delete(Model model, @PathVariable int id) {
+    	
+    	service.deletePerson(id);
     	
     	List<Person> persons = service.getAllPersons();
     	model.addAttribute("persons",persons);
